7accece09ef69c7733fef292ed36ee341ec22be3
CusterFun <custer@email.cn>
2022-04-19 00:04:21 +0800

    log and tracing

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -11,9 +11,28 @@ pub struct FormData {
 }
 
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
-    // `Result` has two variants: `Ok` and `Err`.
-    // The first for successes, the second for failures.
-    // We use a `match` statement to choose what to do based on the outcome.
+    // Let's generate a random unique identifier
+    let request_id = Uuid::new_v4();
+    // Spans, like logs, have an associated level
+    // `info_span` creates a span at the info-level
+    let request_span = tracing::info_span!(
+        "Adding a new subscriber.",
+        %request_id,
+        subscriber_email=%form.email,
+        subriber_name=%form.name
+    );
+    // Using `enter` in an async function is a recipe for disaster!
+    // Bear with me for now, but don't do this at home.
+    // See the following section on `Instrumenting Futures`
+    let _request_span_guard = request_span.enter();
+
+    tracing::info!(
+        "request_id {} - Adding '{}' '{}' as a new subscriber.",
+        request_id,
+        form.email,
+        form.name
+    );
+    tracing::info!("request_id {request_id} - Saving new subscriber details in the database");
     match sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -27,10 +46,17 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
     .execute(pool.as_ref())
     .await
     {
-        Ok(_) => HttpResponse::Ok().finish(),
+        Ok(_) => {
+            tracing::info!("request_id {request_id} - New subscriber details have been saved");
+
+            HttpResponse::Ok().finish()
+        }
         Err(e) => {
-            println!("Failed to execute query {e:?}");
+            tracing::error!("request_id {request_id} - Failed to execute query {e:?}");
             HttpResponse::InternalServerError().finish()
         }
     }
+
+    // `_request_span_guard` is dropped at the end of `subscribe`
+    // That's when we "exit" the span
 }
